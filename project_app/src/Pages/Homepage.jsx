7d2c3d444d700d362c0e12c9f01008a142790df5
Loh Mehul <115700236+LohMehul@users.noreply.github.com>
2023-09-21 13:18:07 +0530

    First pages initialized

---
+++ project_app/src/Pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../home.css"
+const HomePage = () => {
+    return (
+        <>
+            <div className="sec-one">
+            <h2>Welcome to my Home </h2>
+            </div>
+        </>
+    );
+};
+
+export default HomePage;
---
+++ project_app/src/router.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {createBrowserRouter} from "react-router-dom";
+import Home from "./Pages/Homepage.jsx"
+import Header from "./commonPage/HeaderPage.jsx"
+import About from "./Pages/AboutPage.jsx"
+import Footer from "./commonPage./FooterPage.jsx"
+
+const MainRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <><Header/><Home/><Footer/></>,
+  },
+  {
+    path: "/about",
+    element: <><Header/><About/><Footer/></>,
+  },
+]);
+
+
+export default MainRouter;
