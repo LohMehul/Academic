261048b417a6679abeddf49ccb28bd61f8341f99
Loh Mehul <115700236+LohMehul@users.noreply.github.com>
2023-09-22 17:17:38 +0530

    Update Tech_.jsx

--- project_app/src/Pages/Tech_.jsx
+++ project_app/src/Pages/Tech_.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import {
   MDBCard,
+  MDBCardImage,
   MDBCardBody,
+  MDBCardTitle,
   MDBCardText,
-  MDBCardImage
+  MDBCardLink,
+  MDBListGroup,
+  MDBListGroupItem
 } from 'mdb-react-ui-kit';
 
 export default function App() {
   return (
-    <MDBCard>
-      <MDBCardImage src='https://mdbootstrap.com/img/new/standard/nature/182.webp' alt='...' position='top' />
+    <MDBCard className='col-3'>
+      <MDBCardImage position='top' alt='...' src='https://mdbootstrap.com/img/new/standard/city/062.webp' />
       <MDBCardBody>
+        <MDBCardTitle>Card title</MDBCardTitle>
         <MDBCardText>
           Some quick example text to build on the card title and make up the bulk of the card's content.
         </MDBCardText>
+      </MDBCardBody>
+      <MDBListGroup flush>
+        <MDBListGroupItem>Cras justo odio</MDBListGroupItem>
+        <MDBListGroupItem>Dapibus ac facilisis in</MDBListGroupItem>
+        <MDBListGroupItem>Vestibulum at eros</MDBListGroupItem>
+      </MDBListGroup>
+      <MDBCardBody>
+        <MDBCardLink href='#'>Card link</MDBCardLink>
+        <MDBCardLink href='#'>Card link</MDBCardLink>
       </MDBCardBody>
     </MDBCard>
   );
